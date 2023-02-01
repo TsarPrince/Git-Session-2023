455c0a6573244a446a2f2707fb4a522158b4a6d7
Ankit <mahesh9426346889@gmail.com>
2023-02-01 17:23:14 +0530

    ankit

---
+++ Place file here/lcb2022014.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "I am Ankit Yadav" << endl;
+    cout << "roll no. lcb2022014" << endl;
+    cout << " I know coding languages like c++,c,js,bash." << endl;
+    return 0;
+}
