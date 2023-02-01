83637232ecf3d527588f525223329083d1978d95
Anmol <anmolnag576@gmail.com>
2023-02-01 17:37:45 +0530

    removed errors

--- Place file here/lit2022007.cpp
+++ Place file here/lit2022007.cpp
@@ -1,7 +1,7 @@
-#include<iostream.h>
+#include<iostream>
 using namespace std;
 int main()
 {
 for(int i=0;i<5;i++)
-cout<<"Hello I am Anmol Nag from It branch\n"
+cout<<"Hello I am Anmol Nag from It branch\n";
 }
