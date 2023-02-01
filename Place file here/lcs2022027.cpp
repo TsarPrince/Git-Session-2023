c6ba259859f760bc44d95d6b6f22461b0fab0d76
Krishna Sumit <krishnasumit155@gmail.com>
2023-02-01 18:15:44 +0530

    Krishna

---
+++ Place file here/lcs2022027.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Name:Krishna Sumit"<<endl;
+    cout<<"Roll_No:LCS2022027"<<endl;
+    cout<<"Branch:Computer Science"<<endl;
+    return 0;
+}
