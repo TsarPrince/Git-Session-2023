addd3f5c53e738d1d5c1a8084db93a96b0bbda78
unknown <iit2022018@iiitl.ac.in>
2023-02-01 17:31:25 +0530

    Divyansh Gupta LIT2022018

--- Place file here/LIT2022018.cpp
+++ Place file here/LIT2022018.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
     cout<<"I am Divyansh Gupta, IT Fresher student of IIIT Lucknow"<<endl;
-    cout<<"A CREATIVE MAN IS MOTIVATED BYB THE DESIRE TO ACHIEVE \n NOT BY THE DESIRE TO BEAT OTHERS"<<endl;
+    cout<<"A CREATIVE MAN IS MOTIVATED BYB THE DESIRE TO ACHIEVE \n NOT BY THE DESIRE TO BEAT OTHERS"<<endl;`
     return 0;
 }
