e5f509f80eef91ff54c9c193d8ec3f4def5510c6
N.Karthik Akshaj <akshajnk@gmail.com>
2023-02-01 17:28:27 +0530

    Attendance of N.Karthik Akshaj

--- Place file here/LCS2022036.cpp
+++ Place file here/LCS2022036.cpp
@@ -5,6 +5,8 @@ int main(){
     cout<<"My name is N.Karthik Akshaj from CS branch"<<endl;
     cout<<"My roll.no is LCS2022036"<<endl;
     cout<<"Iam from Andhra pradesh"<<endl;
+    cout<<"  "<<endl;
     
+
      return 0;
 }
