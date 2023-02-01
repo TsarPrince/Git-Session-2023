e81dcd6f3c903ac6060989e4e6d9cf046b374576
[Jukuri Shashank] <[shashankjukuri@gmail.com]>
2023-02-01 17:25:39 +0530

    My Intro

---
+++ Place file here/lcb2022033.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Jukuri Shashank\n";
+    cout<<"I have been allotted CSB branch in IIITL\n";
+    cout<<"I am from hyderabad\n";
+    cout<<"Technical languages known are HTML,C,C++,Python\n";
+
+    return 0;
+}
